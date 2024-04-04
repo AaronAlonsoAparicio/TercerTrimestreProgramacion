0da2e3950672141669aaa63050558ecba074f0f9
Aaron <Aaron@DESKTOP-PCBJE6C>
2024-04-04 10:31:27 +0200

    Refactorizacion del la clase main.

--- TercerTrimestre/src/ficherosEjercicios/Ejercicio1Personas.java
+++ TercerTrimestre/src/ficherosEjercicios/Ejercicio1Personas.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 public class Ejercicio1Personas {
 
+	private static final String NombreDirectorio = "directorio";
+
 	public static void main(String[] args) {
 		/**
 		 * Vais a crear la clase Persona, con: - un numero de id - nombre - dni - rol
@@ -26,29 +28,18 @@ public class Ejercicio1Personas {
 		 */
 
 		crearDirectorio();
-		
-		Path rutaArchivoPath = Paths.get("directorio/ficheroPrueba.csv");
 
-		try {
-			Files.createFile(rutaArchivoPath);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Path rutaArchivoPath = RutaDelArchivo();
 
-		String lista;
 		if (Files.exists(rutaArchivoPath)) {
 			ArrayList<String> personas = new ArrayList();
 
 			Persona persona1 = new Persona(1, "Aaron", "31247897H", "Alumno");
 			Persona persona2 = new Persona(2, "Julian", "32443243J", "Alumno");
 			Persona persona3 = new Persona(3, "Miguel", "67565454K", "Alumno");
-			Persona persona4 = new Persona(4,"Cristian","54678094B","Alumno");
+			Persona persona4 = new Persona(4, "Cristian", "54678094B", "Alumno");
 
-			personas.add(persona1.toCSV());
-			personas.add(persona2.toCSV());
-			personas.add(persona3.toCSV());
-			personas.add(persona4.toCSV());
+			personasACsv(personas, persona1, persona2, persona3, persona4);
 
 			try {
 				Files.write(rutaArchivoPath, personas);
@@ -59,31 +50,41 @@ public class Ejercicio1Personas {
 		}
 
 	}
-	
-	
-	
+
+	private static Path RutaDelArchivo() {
+		Path rutaArchivoPath = Paths.get(NombreDirectorio + "/ficheroPrueba.csv");
+
+		try {
+			Files.createFile(rutaArchivoPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rutaArchivoPath;
+	}
+
+	private static void personasACsv(ArrayList<String> personas, Persona persona1, Persona persona2, Persona persona3,
+			Persona persona4) {
+		personas.add(persona1.toCSV());
+		personas.add(persona2.toCSV());
+		personas.add(persona3.toCSV());
+		personas.add(persona4.toCSV());
+	}
 
 	public static void crearDirectorio() {
-		Path directorio = Paths.get("directorio");
-		
+		Path directorio = Paths.get(NombreDirectorio);
+
 		try {
-			if(!Files.exists(directorio)) {
+			if (!Files.exists(directorio)) {
 				Files.createDirectory(directorio);
 				System.out.println("Directorio creado con exito.");
-				
+
 			}
-			
-			
-			
+
 		} catch (Exception excepcion) {
 			System.out.println("El directorio ya existe.");
 		}
-		
-		
+
 	}
-	
-	
-	
-	
 
 }
